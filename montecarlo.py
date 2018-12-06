99ebed971c9cf56d9b1096f014a83c3ff88fb920
Esakkikarthik <36202520+Esakkikarthik@users.noreply.github.com>
2018-12-06 12:11:30 +0100

    Add files via upload

---
+++ montecarlo.py
@@ -0,0 +1,16 @@
+import random
+import numpy as np
+from astropy.table import Table, Column, MaskedColumn
+
+def rollDice():
+    roll = random.randint(1,100)
+    return roll
+
+# Now, just to test our dice, let's roll the dice 100 times. 
+
+x = 0
+while x < 100:
+    result = rollDice()
+    print(result)
+    x+=1
+    ascii.write(result, 'values.txt')
